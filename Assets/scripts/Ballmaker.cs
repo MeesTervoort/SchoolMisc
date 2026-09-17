3195591b91dea38fb743b89b39a535e7e9397400
MeesTervoort <39862@ma-web.nl>
2026-09-17 13:59:10 +0200

    absolute trainwreck (not bad)

--- Assets/scripts/Ballmaker.cs
+++ Assets/scripts/Ballmaker.cs
@@ -7,6 +7,35 @@ public class Ballmaker : MonoBehaviour
     public GameObject ballPrefab;
     private float elapsedTime = 0f;
 
+
+    void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Color color = RandomColor();
+            Vector3 randPos = RandomPosition(-10f, 10f);
+            CreateBall(color, randPos);
+        }
+    }
+
+    void Update()
+    {
+        float r = Random.Range(0f, 1f);
+        float g = Random.Range(0f, 1f);
+        float b = Random.Range(0f, 1f);
+        Color randColor = new Color(r, g, b, 1f);
+
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime > 1f)
+        {
+
+            CreateBall(randColor);
+
+            elapsedTime = 0f;
+
+        }
+
+    }
     private GameObject CreateBall(Color c)
     {
 
@@ -21,69 +50,6 @@ public class Ballmaker : MonoBehaviour
         }
 
         return ball;
-
+        //ill leave this for you future me :3
     }
-
-    void Update()
-    {
-        float r = Random.Range(0f, 1f);
-        float g = Random.Range(0f, 1f);
-        float b = Random.Range(0f, 1f);
-        Color randColor = new Color(r, g, b, 1f);
-
-        elapsedTime += Time.deltaTime;
-        if (elapsedTime > 1f)
-        {
-
-            GameObject ball = CreateBall(randColor);
-            //DestroyBall(ball);
-
-            elapsedTime = 0f;
-
-        }
-
-    }
-
-    //void Start()
-    //{
-
-    //}
-
-    //void Update()
-    //{
-    //    elapsedTime += Time.deltaTime;
-    //    if (elapsedTime > 1f)
-    //    {
-    //        CreateBall(randColor, transform.position);
-    //        elapsedTime = 0f;
-    //    }
-
-    //    float r = Random.Range(0f, 1f);
-    //    float g = Random.Range(0f, 1f);
-    //    float b = Random.Range(0f, 1f);
-    //    Color randColor = new Color(r, g, b, 1f);
-
-    //    elapsedTime += Time.deltaTime;
-
-    //    if (elapsedTime > 1f)
-    //    {
-    //        CreateBall(randColor);
-    //        elapsedTime = 0f;
-    //    }
-    //}
-
-
-    //private void CreateBall(Color c)
-    //{
-
-    //    GameObject ball = Instantiate(ballPrefab);
-    //    Material material = ball.GetComponent<MeshRenderer>().material;
-
-    //    material.SetColor("_Color", c);
-
-    //    if (material.shader.name == "Universal Render Pipeline/Lit")
-    //    {
-    //        material.SetColor("_BaseColor", c);
-    //    }
-    //}
 }
