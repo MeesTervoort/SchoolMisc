81585afeab200f6261bbfac21c80ac8c596a53eb
MeesTervoort <39862@ma-web.nl>
2026-09-24 14:02:56 +0200

    scripting it so goood

--- Assets/ProgOpdrachten/scripts/scripting it.cs
+++ Assets/ProgOpdrachten/scripts/scripting it.cs
@@ -5,6 +5,9 @@ public class scriptingit : MonoBehaviour
     public string size = "Short";
     public int feet = 5;
     public bool isTall = false;
+
+    public int health = 100;
+    public bool Isdead = false;
     void Start()
     {
         Debug.Log("Size: " + size);
@@ -14,6 +17,30 @@ public class scriptingit : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            health -= 35;
+            Debug.Log("Health: " + health);
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            health -= 80;
+            Debug.Log("Health: " + health);
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("Isdead: " + Isdead);
+        }
+
+
+        if (health <= 0)
+        {
+            health = 0;
+            Debug.Log("You are dead");
+            Isdead = true;
+            health = 1;
+        }
     }
 }
